ecc26f5cf1dd2a856af584ecb619fade7431337c
richardmazo <richard.mazo.97@hotmail.com>
2023-04-24 15:06:27 -0500

    adding drone data

--- domain/model/src/main/java/co/com/musala/model/drone/Drone.java
+++ domain/model/src/main/java/co/com/musala/model/drone/Drone.java
@@ -4,15 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Builder(toBuilder = true)
 @AllArgsConstructor
 
 public class Drone {
-
+    private Long id;
     private final String serialNumber;
     private final String model;
     private final Double weightLimit;
-    private final String batteryCapacity;
+    private final BigDecimal batteryCapacity;
     private final String state;
 }
---
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/musala/jpa/drone/DroneData.java
@@ -0,0 +1,42 @@
+package co.com.musala.jpa.drone;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "TBL_DRONE")
+public class DroneData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name="SERIAL_NUMBER",length = 100)
+    private String serialNumber;
+
+    @Column(name = "MODEL")
+    private String model;
+
+    @Column(name = "WEIGHT_LIMIT")
+    private Double weightLimit;
+
+    @Column(name="BATERRY_CAPACITY", precision = 5,  scale = 2)
+    private BigDecimal batteryCapacity;
+
+    @Column(name = "STATE")
+    private String state;
+}
