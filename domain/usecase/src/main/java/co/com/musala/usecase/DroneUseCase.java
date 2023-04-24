1ef091ddf42c4cf2bcf8a04736f4e699ab868028
richardmazo <richard.mazo.97@hotmail.com>
2023-04-24 14:24:16 -0500

    Adding jpa configurations

--- domain/usecase/src/main/java/co/com/musala/usecase/DroneUseCase.java
+++ domain/usecase/src/main/java/co/com/musala/usecase/DroneUseCase.java
@@ -1,5 +1,19 @@
 package co.com.musala.usecase;
 
+import co.com.musala.model.drone.Drone;
+import co.com.musala.model.drone.gateway.DroneRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public class DroneUseCase {
-    
+
+
+    private final DroneRepository droneRepository;
+
+    public List<Drone> getAllDrones(){
+        return droneRepository.findAllDrones();
+    }
+
 }
